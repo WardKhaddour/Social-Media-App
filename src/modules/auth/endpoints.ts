e966953d228987a08a0252fea0fcf28c00ca4510
WardKhaddour <wardkhaddot@gmail.com>
2023-04-15 04:06:18 +0300

    remove unused endpoint

--- src/modules/auth/endpoints.ts
+++ src/modules/auth/endpoints.ts
@@ -1,6 +1,5 @@
 export const SIGNUP = '/auth/signup';
 export const LOGIN = '/auth/login';
 export const FORGOT_PASSWORD = '/auth/forgot-password';
-export const UPDATE_PASSWORD = '/auth/update-password';
 export const RESET_PASSWORD = (token: string) =>
   `/auth/reset-password/${token}`;
