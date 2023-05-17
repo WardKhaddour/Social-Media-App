393996f5b85d49ff5c4bfd6c501e39b9f97ec48a
WardKhaddour <wardkhaddot@gmail.com>
2023-05-17 21:42:28 +0300

    connect socket.io with server correctly

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000');
+const socket = io(process.env.REACT_APP_BASE_URL!);
 socket.on('connect', () => {
   console.log('connected to server');
 });
